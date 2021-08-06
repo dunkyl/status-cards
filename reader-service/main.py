59bc457c38acfa69c92d8d68cef8181f9f6e37b0
Dunkyl <38872099+dunkyl@users.noreply.github.com>
2021-08-06 01:37:01 -0700

    bigger uart timeout

--- reader-service/main.py
+++ reader-service/main.py
@@ -1,13 +1,13 @@
 import serial, board
 from adafruit_pn532.uart import PN532_UART
 
-pn532 = PN532_UART(serial.Serial('/dev/ttyS0', baudrate=115200))
+pn532 = PN532_UART(serial.Serial('/dev/ttyS0', baudrate=115200, timeout=100))
 
 pn532.SAM_configuration()
 
 while True:
-    uid = pn532.read_passive_target()
-    print('.', end="", flush=True)
+    uid = pn532.read_passive_target(timeout=0.5)
+    print('.', end="")
     if uid is None:
         continue
     print('Found card with UID:', '-'.join([hex(i) for i in uid]))
