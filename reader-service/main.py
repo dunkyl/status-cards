8c72b7755877478cb6dc0aa2cbccedd218cbc498
Dunkyl <38872099+dunkyl@users.noreply.github.com>
2021-08-06 01:38:35 -0700

    power down on interrupt

--- reader-service/main.py
+++ reader-service/main.py
@@ -6,8 +6,11 @@ pn532 = PN532_UART(serial.Serial('/dev/ttyS0', baudrate=115200, timeout=100))
 pn532.SAM_configuration()
 
 while True:
-    uid = pn532.read_passive_target(timeout=0.5)
-    print('.', end="")
-    if uid is None:
-        continue
-    print('Found card with UID:', '-'.join([hex(i) for i in uid]))
+    try:
+        uid = pn532.read_passive_target(timeout=0.5)
+        print('.', end="")
+        if uid is None:
+            continue
+        print('Found card with UID:', '-'.join([hex(i) for i in uid]))
+    except KeyboardInterrupt:
+        pn532.power_down()
