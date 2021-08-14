032a65017698fe9071698fb9bdc43274b23db7a6
Dunkyl <38872099+dunkyl@users.noreply.github.com>
2021-08-14 15:49:51 -0700

    lights

--- reader-service/main.py
+++ reader-service/main.py
@@ -12,18 +12,48 @@ pool = ThreadPoolExecutor()
 connections: list[WebSocketServerProtocol] = []
 reader_task = None
 
-def dout(pin):
+class Status:
+    Unknown = -1
+    Online = 0
+    Away = 1
+    DND = 2
+    Invisible = 3
+    Custom = 4
+
+CARDS = {
+    '8a-f6-3d-98': Status.Online,
+    '23-49-81-52': Status.Away,
+    '3a-ff-3b-98': Status.DND,
+    'fa-7f-3c-98': Status.Invisible
+}
+
+def dout(pin, value=0):
     p = digitalio.DigitalInOut(pin)
     p.direction = digitalio.Direction.OUTPUT
+    p.value = value
     return p
 
-status_led = dout(board.D17)
-status_led.value = 0
+LEDS = {
+    'g': dout(board.D27),
+    'w': dout(board.D17),
+    'r': dout(board.D22),
+    'p': dout(board.D16),
+    'b': dout(board.D12),
+}
+LEDS_I = list(LEDS.items())
+
+async def clear_leds_to(x):
+    for i in range(len(LEDS_I)):
+        LEDS_I[i].value = 1
+        await asyncio.sleep(0.25)
+    for i in range(len(LEDS_I)):
+        LEDS_I[i].value = x == i
+        await asyncio.sleep(0.25)
 
 async def blink():
-    status_led.value = 1
+    LEDS['w'].value = 1
     await asyncio.sleep(0.25)
-    status_led.value = 0
+    LEDS['w'].value = 0
 
 async def card_read(pn532):
     #status_led.value = 1
@@ -42,12 +72,13 @@ async def card_read_loop():
                     uid = await card_read(pn532)
                     if not uid:
                         continue
-                    status_led.value = 1
                     uid_str = '-'.join(hex(i)[2:] for i in uid)
                     print(F"Card: {uid_str}")
                     for ws in list(connections):
                         await ws.send(uid_str)
-                    status_led.value = 0
+                    await blink()
+                    status = CARDS[uid_str]
+                    await clear_leds_to(status)
             except RuntimeError as e:
                 uart.close()
                 await asyncio.sleep(1)
