55efc0a2401cd7527590561c819633508fbb1de9
Dunkyl <38872099+dunkyl@users.noreply.github.com>
2022-05-02 04:05:26 -0700

    test

---
+++ reader-service/test-lights.py
@@ -0,0 +1,37 @@
+import serial, asyncio
+from concurrent.futures import ThreadPoolExecutor
+import traceback
+import board
+import digitalio
+from sqlalchemy import true
+
+def dout(pin, value=0):
+    p = digitalio.DigitalInOut(pin)
+    p.direction = digitalio.Direction.OUTPUT
+    p.value = value
+    return p
+
+LEDS = {
+    'g': dout(board.D6),
+    'w': dout(board.D17),
+    'r': dout(board.D22),
+    'p': dout(board.D16),
+    'b': dout(board.D12),
+}
+LEDS_I = list(LEDS.values())
+
+async def blink(led='w'):
+    LEDS[led].value = 1
+    await asyncio.sleep(0.25)
+    LEDS[led].value = 0
+
+
+
+async def main():
+    
+    while True:
+        for led in LEDS:
+            await blink(led)
+        await asyncio.sleep(0.25)
+    
+asyncio.run(main())
