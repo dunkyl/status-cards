bb444d6f3222d4f1c2104b4116a2227e5dac7daa
Dunkyl <38872099+dunkyl@users.noreply.github.com>
2023-10-01 03:37:20 -0700

    test card read script

---
+++ reader-service/test-cards.py
@@ -0,0 +1,13 @@
+import serial
+from adafruit_pn532.uart import PN532_UART
+
+uart = serial.Serial('/dev/serial1', baudrate=115200, timeout=1)
+pn532 = PN532_UART(uart, debug=False)
+pn532.SAM_configuration()
+try:
+    while True:
+        uid = pn532.read_passive_target()
+        print(uid)
+finally:
+    pn532.power_down()
+    uart.close()
